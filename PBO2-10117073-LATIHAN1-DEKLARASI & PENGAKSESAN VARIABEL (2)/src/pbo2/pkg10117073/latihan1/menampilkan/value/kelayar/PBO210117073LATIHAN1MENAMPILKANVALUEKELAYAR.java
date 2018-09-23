5ae8a4b8b28a6707af3683fb46a1c4e9ee1fe09d
ajipurnomo <aji.purnomo30399@gmail.com>
2018-09-23 20:03:32 +0700

    Praktek1

---
+++ PBO2-10117073-LATIHAN1-DEKLARASI & PENGAKSESAN VARIABEL (2)/src/pbo2/pkg10117073/latihan1/menampilkan/value/kelayar/PBO210117073LATIHAN1MENAMPILKANVALUEKELAYAR.java
@@ -0,0 +1,33 @@
+/*
+ * NAMA       : AJI PURNOMO
+ * KELAS      : IF-2
+ * MATAKULIAH : PBO2
+ * NIM        : 10117073
+ * DESKRIPSI PROGRES : Program ini berisi program untuk menampilkan value 
+ * ke layar
+ */
+package pbo2.pkg10117073.latihan1.menampilkan.value.kelayar;
+
+/**
+ *
+ * @author ZeroXploit
+ */
+public class PBO210117073LATIHAN1MENAMPILKANVALUEKELAYAR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIDATA ROCKSTAR" + "\n" + "------------------------");
+        System.out.println("NAMA\t\t: Alwi Yahya Muljabar");
+        System.out.println("ALAMAT\t\t: JL.SEKELOA GG.LOA 1 NO 30 RT/RW 01/02");
+        System.out.println("TEMPAT LAHIR\t: Tasikmalaya");
+        System.out.println("TANGGAL LAHIR\t: 20 Oktober 2018");
+        System.out.println("------------------------");
+        System.out.println("HOBI\t\t: Bermain Game & Futsall");
+        System.out.println("MAKES\t\t: Sate & Nasi Goreng");
+        System.out.println("MIKES\t\t: Cendol & ES Teh Manis");
+    }
+    
+}
