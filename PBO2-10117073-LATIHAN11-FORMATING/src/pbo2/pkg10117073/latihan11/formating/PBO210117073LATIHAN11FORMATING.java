ab9f71b5715a1c0daa6cf597ac407626ec407614
ajipurnomo <aji.purnomo30399@gmail.com>
2018-09-23 19:56:48 +0700

    Praktek1

---
+++ PBO2-10117073-LATIHAN11-FORMATING/src/pbo2/pkg10117073/latihan11/formating/PBO210117073LATIHAN11FORMATING.java
@@ -0,0 +1,30 @@
+/*
+ * NAMA       : AJI PURNOMO
+ * KELAS      : IF2
+ * MATAKULIAH : PBO2
+ * NIM        : 10117073
+ * DESKRIPSI PROGRES : Program ini berisi program penggunaan formating
+ */
+package pbo2.pkg10117073.latihan11.formating;
+
+public class PBO210117073LATIHAN11FORMATING {
+
+    public static void main(String[] args) {
+        int i = 3546764;
+        int iMinus = -i;
+        System.out.println("i : " + i);
+        System.out.printf("%%d : %d%n", i);
+        System.out.printf("%%10d : %10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", i);
+        System.out.printf("%%+10d : %+10d%n", iMinus);
+        System.out.printf("%%,10d : %,10d%n", i);
+        System.out.printf("%%-10d : %-10d%n", i);
+        double f = 5675482.982;
+        System.out.println("f : f" + f);
+        System.out.printf("%%f : %f%n", f);
+        System.out.printf("%%.2f : %.2f%n", f);
+        System.out.printf("%%12.2f : %12.2f%n", f);
+        System.out.printf("%%,12.2f : %,12.2f%n", f);
+    }
+    
+}
---
+++ PBO2-10117073-LATIHAN8-PECAHAN/src/pbo2/pkg10117073/latihan8/pecahan/PBO210117073LATIHAN8PECAHAN.java
@@ -0,0 +1,24 @@
+/*
+ * NAMA       : AJI PURNOMO
+ * KELAS      : IF2
+ * MATAKULIAH : PBO2
+ * NIM        : 10117073
+ * DESKRIPSI PROGRES : Program ini menampilkan data bilangan pecahan
+ */
+package pbo2.pkg10117073.latihan8.pecahan;
+
+public class PBO210117073LATIHAN8PECAHAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("float : " + f);
+        System.out.println("double : " + d);
+    }
+    
+}
